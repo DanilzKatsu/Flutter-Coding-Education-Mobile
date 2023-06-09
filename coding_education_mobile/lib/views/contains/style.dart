ea6cc4866d17aca49c4a8fa3f9ae82789bd597aa
danilz-2706 <Pk8266328>
2023-06-09 12:07:35 +0700

    create structure

---
+++ coding_education_mobile/lib/views/contains/style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color.fromRGBO(21, 21, 34, 1);
+const Color kSecondaryColor = Color.fromRGBO(40, 40, 54, 1);
+const Color kGreyColor = Color.fromRGBO(124, 123, 146, 1);
+const Color kBlueColor = Color.fromRGBO(87, 102, 243, 1);
+const Color kGreenColor = Color.fromRGBO(112, 207, 203, 1);
+const Color kWhiteColor = Color.fromRGBO(240, 240, 240, 1);
+
+const Color kBlurColor = Color.fromRGBO(255, 255, 255, 0.5);
+
+const double kDefaultSpacing = 4.0;
+
+const double kBorder = 8.0;
